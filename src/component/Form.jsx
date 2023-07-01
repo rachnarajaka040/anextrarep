1d5a1806cddb361d47d6e7d87224c1db8b6971d2
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-07-01 13:16:14 +0530

    add

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useState } from 'react';
+import './Signup.css';
 function Form() {
     const [formData, setFormData] = useState({});
-    const navigate = useNavigate();
     const handleChange = (event) => {
         const { name, value } = event.target;
         setFormData({ ...formData, [name]: value });
@@ -10,9 +10,12 @@ function Form() {
     const handleSubmit = (event) => {
         event.preventDefault();
         localStorage.setItem('myFormData', JSON.stringify(formData));
-        document.getElementById('email').value = "";
+        document.getElementById('username').value = "";
+        document.getElementById('age').value = "";
         document.getElementById('password').value = "";
-        navigate('/profile');
+        document.getElementById('email').value = "";
+        document.getElementById('file').value = "";
+       
     };
 
     return (
@@ -22,7 +25,7 @@ function Form() {
                     <div class="col-lg-4 col-md-6 col-sm-6">
                         <div class="card shadow">
                             <div class="card-title text-center border-bottom">
-                                <h2 class="p-3">LoginUp to Your Popx account</h2>
+                                <h2 class="p-3">Form</h2>
                             </div>
                             <div class="card-body">
                                 <form onSubmit={handleSubmit}>
@@ -31,13 +34,22 @@ function Form() {
                                         <input type="text" class="form-control" onChange={handleChange} id="username" name="username" />
                                     </div>
                                     <div class="mb-4">
-                                        <label for="username" class="form-label">Email</label>
-                                        <input type="email" class="form-control" name="email" id="email" onChange={handleChange} />
+                                        <label for="age" class="form-label">Age</label>
+                                        <input type="number" class="form-control" name="age" id="age" onChange={handleChange} />
                                     </div>
                                     <div class="mb-4">
-                                        <label for="password" class="form-label">Password</label>
-                                        <input type="password" onChange={handleChange} class="form-control" id="password" name="password" />
+                                        <label for="weight" class="form-label">Weight</label>
+                                        <input type="number" onChange={handleChange} class="form-control" id="password" name="password" />
                                     </div>
+                                    <div class="mb-4">
+                                        <label for="email" class="form-label">Email</label>
+                                        <input type="email" onChange={handleChange} class="form-control" id="email" name="email" />
+                                    </div>
+                                    <div class="mb-4">
+                                        <label for="file" class="form-label">File</label>
+                                        <input type="file" onChange={handleChange} class="form-control" id="file" name="file" />
+                                    </div>
+                                   
                                     <div class="d-grid gap-2 col- mx-auto" style={{ width: "18rem" }}>
                                         <button class="btn btn-primary" type="submit">Login</button>
 
